6130313eeee7c0b033a17646246139f0602a25a5
chaniWeiner <Hand4c8@mbjcomp.org.il>
2023-11-26 15:45:51 +0200

    start keyboard

--- src/Components/Keyboard/CreateKeyboard.jsx
+++ src/Components/Keyboard/CreateKeyboard.jsx
@@ -1,28 +1,31 @@
 import React from 'react'
-let hebrewKeys=["/","'","ק","ר","א","ט","ו","ן","ם","פ","ש","ד",
+let hebrewKeys=["/","'","ק","ר","א","ט","ו","ן","ם","פ",'back space',"ש","ד",
 "ג","כ","ע","י","ח","ל","ך",
-"ף","ס","ב","ה","נ","מ","צ","ת","ץ","."]
+"ף","shift","ס","ב","ה","נ","מ","צ","ת","ץ","."]
 
-let smallEnglishKeyboard=['/',"'",'q','w','r','t','y','u','i','o','p',
-'a','s','d','f','g','h','j','k','l',
+let smallEnglishKeyboard=['/',"'",'q','w','r','t','y','u','i','o','p','back space',
+'a','s','d','f','g','h','j','k','l',"shift",
 'z','x','c','v','b','n','m','.'];
 
 
-let BigEnglishKeyboard=['/',"'",'Q','W','R','T','Y','U','I','O','P',
-'A','S','D','F','G','H','J','K','L',
+const BigEnglishKeyboard=['/',"'",'Q','W','R','T','Y','U','I','O','P','back space',
+'A','S','D','F','G','H','J','K','L',"shift",
 'Z','X','C','V','B','N','M','.'];
 
 
 function CreateKeyboard()
 {
+    const intalizationKeyboard= smallEnglishKeyboard.map(element =>
+        <button key={element}> {element} </button>
+    );
     return(
         <>
-        {
-            hebrewKeys.forEach(element=> {
-                <button>element</button>
-            })
-        }  
+        {intalizationKeyboard}
         </>
     )
+}
+
+function fun(arr){
+
 }
 export default CreateKeyboard;
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
-import CreateKeyboard from './Components/Keyboard/CreateKeyboard.jsx'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <CreateKeyboard />
+
     <App />
     
   </React.StrictMode>,
